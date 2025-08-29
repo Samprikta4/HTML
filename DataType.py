3b5600d84f10460a0d949a25ae0207bc42e0b25c
Samprikta4 <ssameersarker@gmail.com>
2025-08-29 18:54:19 +0600

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,21 @@
+name = "robert" #string
+age = 15 #integer
+is_student = True #boolean
+weight = 38.5 #float
+
+print("name is :",name)
+print("Data type of name is," ,type(name))
+print(" age:", age)
+print("Data type of age is", type(age))
+print()
+
+
+print ("before Type Casting......")
+print (age)
+print ("Data type of age is", type(age))
+print ()
+print ("After Type Casting.....")
+age = str(age)
+print(age)
+print ("Data type of age is", type(age))
+
