463baea7910ceb5b96fefc08f1d7bcb71e41fa9e
Samprikta4 <ssameersarker@gmail.com>
2025-08-29 19:06:46 +0600

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,16 @@
+num1 = 25
+num2 = 3
+
+print("Aditon:",num1+num2)
+print("FloorDivistion:",num1//num2)
+print("Modulus operation:",num1%num2)
+
+
+print("Square:",num2**2)
+print("Square root:",num1**0.5)
+
+print(num1==num2)
+print(num1>num2)
+
+result=num1/2+num2**2+10
+print("Result of given equation is:",result)
